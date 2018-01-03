a86dcf302044f07f5dea0fe6950efc564f493778
Max Barnell <max.barnell@gmail.com>
2018-01-03 12:12:59 +0100

    Added funcionality to playState

--- src/com/company/PlayState.java
+++ src/com/company/PlayState.java
@@ -1,6 +1,35 @@
 package com.company;
 
 import java.awt.event.KeyEvent;
+import java.io.IOException;
 
 public class PlayState {
+    public static PlayState playState = null;
+    
+    public void init() throws IOException {
+    
+    }
+    
+    public void handleEvents() {
+    
+    }
+    
+    public void update() throws IOException {
+    
+    }
+    
+    public void draw() throws IOException {
+    
+    }
+    
+    protected PlayState() {
+    
+    }
+    
+    public static PlayState getInstance() {
+        if(playState == null) {
+            playState = new PlayState();
+        }
+        return playState;
+    }
 }

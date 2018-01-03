c190377db64a774acbe845e620225f67d83537db
Max Barnell <max.barnell@gmail.com>
2018-01-03 11:44:05 +0100

    Moved init function to board

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -1,4 +1,21 @@
 package com.company;
 
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
+import java.io.IOException;
+
 public class Board {
+    private static Terminal terminal;
+    
+    public Board() throws IOException {
+        terminal = new DefaultTerminalFactory().createTerminal();
+    }
+    
+    public static Terminal getTerminal() throws IOException {
+        /*if(terminal == null) {
+            terminal = new DefaultTerminalFactory().createTerminal();
+        }*/
+        return terminal;
+    }
 }
--- src/com/company/MenuState.java
+++ src/com/company/MenuState.java
@@ -17,27 +17,28 @@ public class MenuState extends GameState {
 
     @Override
     public void update(GameEngine game) throws IOException {
-        InitState.getTerminal().enterPrivateMode();
-        InitState.getTerminal().setCursorPosition(10,10);
-        InitState.getTerminal().putCharacter('M');
-        InitState.getTerminal().putCharacter('e');
-        InitState.getTerminal().putCharacter('n');
-        InitState.getTerminal().putCharacter('u');
-        InitState.getTerminal().setCursorPosition(10, 11);
-        InitState.getTerminal().putCharacter('P');
-        InitState.getTerminal().putCharacter('l');
-        InitState.getTerminal().putCharacter('a');
-        InitState.getTerminal().putCharacter('y');
-        InitState.getTerminal().setCursorPosition(10, 12);
-        InitState.getTerminal().putCharacter('Q');
-        InitState.getTerminal().putCharacter('u');
-        InitState.getTerminal().putCharacter('i');
-        InitState.getTerminal().putCharacter('t');
+        Board.getTerminal().enterPrivateMode();
+        Board.getTerminal().enterPrivateMode();
+        Board.getTerminal().setCursorPosition(10,10);
+        Board.getTerminal().putCharacter('M');
+        Board.getTerminal().putCharacter('e');
+        Board.getTerminal().putCharacter('n');
+        Board.getTerminal().putCharacter('u');
+        Board.getTerminal().setCursorPosition(10, 11);
+        Board.getTerminal().putCharacter('P');
+        Board.getTerminal().putCharacter('l');
+        Board.getTerminal().putCharacter('a');
+        Board.getTerminal().putCharacter('y');
+        Board.getTerminal().setCursorPosition(10, 12);
+        Board.getTerminal().putCharacter('Q');
+        Board.getTerminal().putCharacter('u');
+        Board.getTerminal().putCharacter('i');
+        Board.getTerminal().putCharacter('t');
     }
 
     @Override
     public void draw(GameEngine game) throws IOException {
-        InitState.getTerminal().flush();
+        Board.getTerminal().flush();
     }
 
     protected MenuState() {

5d3996272e63ccd90c0081d1c1d55ea8e85dd9bb
Max Barnell <max.barnell@gmail.com>
2018-01-08 14:36:32 +0100

    Tog bort magiska siffror

--- src/com/company/Board.java
+++ src/com/company/Board.java
@@ -8,26 +8,26 @@ import java.io.IOException;
 
 public class Board extends Object{
     private static Terminal terminal;
+    private static int origin;
+    private static int offset;
     
     public Board() throws IOException {
         terminal = new DefaultTerminalFactory().setInitialTerminalSize(new TerminalSize(100, 30)).createTerminal();
         terminal.setCursorVisible(false);
+        origin = 0;
+        offset = 4;
     }
 
     public static Terminal getTerminal() throws IOException {
         return terminal;
     }
 
-    public static int getRows() throws IOException {
-        int row;
-        row = terminal.getTerminalSize().getRows();
-        return row;
+    public static int borderOrigin() throws IOException {
+        return origin;
     }
 
-    public static int getColumns() throws IOException {
-        int col;
-        col = terminal.getTerminalSize().getColumns();
-        return col;
+    public static int borderOffset() throws IOException {
+        return offset;
     }
     
     public static void printField() throws IOException {

890068a67dd1505ef09818a8a8a142ff35db032f
Max Barnell <max.barnell@gmail.com>
2018-01-08 15:18:37 +0100

    Fixade Dannys misstag

--- src/com/company/Collision.java
+++ src/com/company/Collision.java
@@ -8,38 +8,39 @@ public class Collision {
     public static boolean isEnemyCollisionDetected(Enemy enemy, Player player) {
         return enemy.getX() == player.getX() && enemy.getY() == player.getY();
     }
-
+    
     public static boolean isBitcoinCollisionDetected(Player player, Bitcoin bitcoin) {
         return player.getY() == bitcoin.getY() && player.getX() == bitcoin.getX();
     }
-
-
-    public static boolean isBitcoinCollisionDetected(Enemy enemy, Bitcoin bitcoin) {
-
-
     
-    public static boolean isOutofBounds(IEntity entity) throws IOException {
-
-        boolean isOutofBounds = false;
-<<<<<<< HEAD
-
-        if(player.getX() >= Board.getTerminal().getTerminalSize().getColumns() - 1) {
-=======
+    public static boolean isBitcoinCollisionDetected(Enemy enemy, Bitcoin bitcoin) {
+        return enemy.getY() == bitcoin.getY() && enemy.getX() == bitcoin.getX();
+    }
+    
+    /*public boolean isBothEnemyCollisionDetected(Enemy[] enemy) {
+        boolean isCollision = false;
+        for(int i = 0; i < enemy.length; ++i) {
         
-        if(entity.getX() >= Board.getTerminal().getTerminalSize().getColumns() - 1) {
->>>>>>> 653dbb729783e899965f17caf53000ac1b2cc42e
+        }
+        return en
+    }*/
+    
+    public static boolean isOutofBounds(Player player) throws IOException {
+        boolean isOutofBounds = false;
+        
+        if(player.getX() >= Board.getTerminal().getTerminalSize().getColumns() - 1) {
             isOutofBounds = true;
         }
-        if(entity.getY() >= Board.getTerminal().getTerminalSize().getRows() - 1) {
+        if(player.getY() >= Board.getTerminal().getTerminalSize().getRows() - 1) {
             isOutofBounds = true;
         }
-        if(entity.getX() <= 0) {
+        if(player.getX() <= 0) {
             isOutofBounds = true;
         }
-        if(entity.getY() <= 0) {
+        if(player.getY() <= 0) {
             isOutofBounds = true;
         }
-
+        
         return isOutofBounds;
     }
 }

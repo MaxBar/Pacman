49a8a6b481a7966e10b1226a1a69c4831d001aa7
LauresLindroos <laurea.lindroos@hotmail.com>
2018-01-09 14:45:24 +0100

    playstate points

--- src/com/company/GameModes/PlayState.java
+++ src/com/company/GameModes/PlayState.java
@@ -3,6 +3,7 @@ package com.company.GameModes;
 import com.company.Bitcoin;
 import com.company.Board;
 import com.company.Entities.*;
+import com.company.GameScore;
 import com.company.States.GameEngine;
 import com.company.States.GameState;
 import com.googlecode.lanterna.input.KeyStroke;
@@ -19,6 +20,7 @@ public class PlayState extends GameState {
     private Bitcoin bitcoin;
     private int amount;
     private Wall wall;
+    private GameScore points;
     
     @Override
     public void init() throws IOException {
@@ -91,6 +93,7 @@ public class PlayState extends GameState {
         wall.update();
         player.update();
         bitcoin.update();
+        points.update();
         for (int i = 0; i < enemies.length; ++i) {
             enemies[i].update();
 

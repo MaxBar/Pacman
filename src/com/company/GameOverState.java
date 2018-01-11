11853c07f31dfab3fba59588dfe3643c9b39c064
LauresLindroos <laurea.lindroos@hotmail.com>
2018-01-11 15:39:08 +0100

    Further changers in Game Over

--- src/com/company/GameOverState.java
+++ src/com/company/GameOverState.java
@@ -1,12 +1,19 @@
 package com.company;
 
 import com.company.GameModes.MenuState;
+import com.company.GameModes.PlayState;
 import com.company.States.GameEngine;
 import com.company.States.GameState;
+import com.googlecode.lanterna.input.KeyStroke;
 
 import java.io.IOException;
 
+import static com.googlecode.lanterna.input.KeyType.Escape;
+import static sun.tools.java.Scanner.EOF;
+
 public class GameOverState extends GameState {
+    private KeyStroke key;
+    private static GameOverState gameOverState = null;
 
     @Override
     public void init() throws IOException {
@@ -14,13 +21,28 @@ public class GameOverState extends GameState {
         Board.getTerminal().enterPrivateMode();
         Board.getTerminal().newTextGraphics().putString((Board.getTerminal().getTerminalSize().getColumns() / 2) - 9, (Board.getTerminal().getTerminalSize().getRows() / 2) - 1, "GAME OVER");
     }
-<<<<<<< HEAD
-    public void gameOver(GameEngine game) throws IOException {
-=======
 
     @Override
     public void handleEvents(GameEngine game) throws IOException {
-
+        key = Board.getTerminal().readInput();
+        switch (key.getKeyType()) {
+            case Escape:
+                game.quit();
+                break;
+            case EOF:
+                game.quit();
+                break;
+            case Character:
+                switch (key.getCharacter()) {
+                    case 'p':
+                        Board.getTerminal().clearScreen();
+                        game.changeState(PlayState.getInstance());
+                        break;
+                    case 'q':
+                        game.quit();
+                        break;
+                }
+        }
     }
 
     @Override
@@ -34,7 +56,12 @@ public class GameOverState extends GameState {
     }
 
     public void GameOver(GameEngine game) throws IOException {
->>>>>>> 8307b70e45630508daad562ae9383c476b405c68
         game.changeState(MenuState.getInstance());
+    }
+    public static GameOverState getInstance() {
+        if (gameOverState == null) {
+            gameOverState = new GameOverState();
+        }
+        return gameOverState;
     }
 }

7ffb772ce9ca9a92a9ee11e86b27f9fda70019a9
Max Barnell <max.barnell@gmail.com>
2018-01-12 11:16:24 +0100

    Changed main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,21 +8,7 @@ import java.io.IOException;
 public class Main {
     
     public static void main(String[] args) throws InterruptedException, IOException {
-        GameEngine game = new GameEngine();
-        
-        game.init();
-        
-        game.changeState(InitState.getInstance());
-        
-        while (game.running()) {
-
-            if(DeltaTime.getDeltaTime() > DeltaTime.getFPS()) {
-                game.handleEvents();
-                game.update();
-                game.draw();
-                DeltaTime.setDeltaTime(0);
-            }
-        }
-        Board.getTerminal().exitPrivateMode();
+        Application app = new Application();
+        app.run();
     }
 }
